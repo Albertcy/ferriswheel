6eafa2c668b644d079dee09eb0a921c3c7cd633b
Albert <bertcy@qq.com>
2016-05-29 13:00:04 +0800

    update method

--- src/algorithm_lib/vb.php
+++ src/algorithm_lib/vb.php
@@ -1,32 +1,50 @@
 <?php
+
 /**
  * @date 2016-05-23
  */
 /**
  * variable byte code encode
- * @param int $arg
+ *
+ * @param int $arg            
  * @return int
  */
-function vbEncode($arg) {
+function vbEncode($arg)
+{
     $ret = null;
     $i = 0;
-    while ( $arg > 127 ) {
-        $ret = $ret | (($arg & 127 | 128) << (8 * $i ++));
+    while ($arg > 127) {
+        // $ret = $ret | (($arg & 127 | 128) << (8 * $i ++));
+        $i ++;
+        $ret = pack('C', ($arg & 127 | 128)) . $ret;
         $arg >>= 7;
     }
-    $i > 0 ? ($ret |= $arg << (8 * $i)) : ($ret = $arg | 128);
+    // $i > 0 ? ($ret |= $arg << (8 * $i)) : ($ret = $arg | 128);
+    $i > 0 ? ($ret = pack('C', $arg) . $ret) : ($ret = pack('C', $arg));
     return $ret;
 }
 
 /**
  * variable byte code decode
- * @param int $arg
+ *
+ * @param int $arg            
  * @return int
  */
-function vbDecode($arg) {
+function vbDecode($arg)
+{
+    $ret = null;
+    $bits = unpack('C*', $arg);
+    $slen = count($bits);
+    foreach($bits as $byte){
+        $ret |= ($byte & 127 )<< (7 * --$slen);
+    }
+    return $ret;
+}
+
+function vbDecodebit($arg){
     $ret = null;
     $i = 0;
-    while ( 128 == ($arg & 128) ) {
+    while (128 == ($arg & 128)) {
         $ret |= $arg & 127 << (7 * $i ++);
         $arg >>= 8;
     }
