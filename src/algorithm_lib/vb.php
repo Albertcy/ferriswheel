bf66fb29dec9f405086fa418d13936fd9094d6f8
Albert <bertcy@qq.com>
2016-05-30 00:51:43 +0800

    add file encoding demo

--- src/algorithm_lib/vb.php
+++ src/algorithm_lib/vb.php
@@ -34,7 +34,7 @@ function vbDecode($arg)
     $i = - 1;
     $bits = unpack('C*', $arg);
     foreach ($bits as $byte) {
-        0 == ($byte & 128) ? ++ $i : ($ret[$i] <<= 7);
+        0 == ($byte & 128) ? ($ret[++ $i] = null) : ($ret[$i] <<= 7);
         $ret[$i] |= ($byte & 127);
     }
     return $ret;
@@ -75,8 +75,48 @@ function vbDecodebit($arg)
     return $ret;
 }
 
-$enNum = vbEncode(12238) . vbEncode(2323) . vbEncode(35232) .vbEncode(23423);
-print_r(vbDecode($enNum));
-echo strlen($enNum);
+function encodeFile($file)
+{
+    $handleR = @fopen("$file", "rb");
+    $outputFile = 'encode_' . time() . '.txt';
+    $handleW = @fopen($outputFile, 'wb');
+    if ($handleR) {
+        while (($line = fgets($handleR, 4096)) !== false) {
+            $strings = explode(' ', $line);
+            $nums = explode(',', $strings[1]);
+            $encodeNums = '';
+            foreach ($nums as $num) {
+                $encodeNums .= vbEncode($num);
+            }
+            $outputLine = pack('N2', strlen($strings[0]), strlen($encodeNums)) . $strings[0] . $encodeNums;
+            fwrite($handleW, $outputLine);
+        }
+        if (! feof($handleR)) {
+            echo "Error: unexpected fgets() fail\n";
+        }
+        fclose($handleR);
+        fclose($handleW);
+    }
+}
+
+function decodeFile($file)
+{
+    $handleR = @fopen($file, 'rb');
+    $i = 0;
+    while (! feof($handleR) && '' != ($buffer = fread($handleR, 8))) {
+        $i ++;
+        $lens = unpack('N2', $buffer);
+        $tag = fread($handleR, $lens[1]);
+        $numsBits = fread($handleR, $lens[2]);
+        $nums = implode(',', vbDecode($numsBits));
+        $line =  "{$tag} {$nums}" . PHP_EOL;
+        file_put_contents('source_' . time() . 'txt.', $line , FILE_APPEND);
+    }
+}
+
+//encodeFile('text.txt');
+//decodeFile('encode_1464540523.txt');
+
+
 
 
